dbdaaa247092cf3a8f8279c72ef3ced13c86feb6
Vaibhav Goyal <goyalvaibhav004@gmail.com>
2025-04-11 10:07:38 +0530

    Add CORS support for Flutter

--- django_api/settings.py
+++ django_api/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'students',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
