759b03e90af3aa3cf6a4037c5e7e41a7895dce98
Vaibhav Goyal <goyalvaibhav004@gmail.com>
2025-04-16 12:50:52 +0530

    Explicitly define all fields in serializers for better UI

--- students/serializers.py
+++ students/serializers.py
@@ -4,9 +4,9 @@ from .models import SecondYearStudent, ThirdFourthYearStudent
 class SecondYearStudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = SecondYearStudent
-        fields = '__all__'
+        fields = ['student_id', 'first_name', 'last_name', 'major', 'email', 'section', 'practicum_details']
 
 class ThirdFourthYearStudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = ThirdFourthYearStudent
-        fields = '__all__'
+        fields = ['student_id', 'first_name', 'last_name', 'major', 'email', 'section', 'internship_details']
